c662c3a7d2ef441c22ce4acd07682372c880821e
austin <aguspdc@gmail.com>
2023-02-23 08:40:27 -0300

    Revert Firebase config and files

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Dependencies
+var fb = require("firebase/app");
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var url = require('url');
 var fs = require('fs');
-var fb = require("firebase/app");
+
 // import * as firebase from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 
 // Local Modules
@@ -11,6 +13,7 @@ var fb = require("firebase/app");
 
 // Server Initialization
 var app = express();
+app.use(cors());
 app.use(express.static('src'));
 
 
@@ -21,7 +24,6 @@ app.get('/', (req, res) => {
 app.get('/sim', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/pages/sim.html'))
 });
-
 
 // Server Listen Along with Database 
 // connection(in case of data persistence)
@@ -40,13 +42,13 @@ function listen() {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAxmgGPPMaRY5jtd5lNxNZfSrs02pkFI-M",
-    authDomain: "capstone-sim.firebaseapp.com",
-    projectId: "capstone-sim",
-    storageBucket: "capstone-sim.appspot.com",
-    messagingSenderId: "912739197756",
-    appId: "1:912739197756:web:afc9b2b199b8e6d5d17439",
-    measurementId: "G-VM7ZMJMDFV"
+    apiKey: "AIzaSyDcxnL7kvyKbFkB6F88hGieRY8xPKTvAaU",
+    authDomain: "capstone-simdev.firebaseapp.com",
+    projectId: "capstone-simdev",
+    storageBucket: "capstone-simdev.appspot.com",
+    messagingSenderId: "715734523256",
+    appId: "1:715734523256:web:2dad63ac2ca7b0a6bdbc8d",
+    measurementId: "G-KK7SEZN6F5"
 };
 
 // Initialize Firebase
