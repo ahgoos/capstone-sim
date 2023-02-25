7bef6c4cbde4ac078259c6c5c43571c1d04a0401
austin <aguspdc@gmail.com>
2023-02-25 10:27:21 -0300

    Add draggable cards and interactive sim gui setup

--- dnd.js
+++ dnd.js
@@ -1,65 +1,70 @@
-
+///////////////////////////////////////////
+///////////  Drag and Drop  ///////////////
+///////////////////////////////////////////
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    let offset = [0, 0];
+    let isDown = false;
+    let cards = document.querySelectorAll('.card');
+    cards.forEach(function (card) {
 
-    var dragSrcEl = null;
+        card.addEventListener('dragstart', function (e) {
+            e.dataTransfer.setDragImage(card, -9999, -9999);
+            e.target.classList.add('dragging');
+            isDown = true;
+            card.style.position = "absolute";
+            offset = [
+                card.offsetLeft - e.clientX,
+                card.offsetTop - e.clientY
+            ];
+            // console.log("start:", offset);
 
-    function handleDragStart(e) {
-        this.style.opacity = '0.4';
-
-        dragSrcEl = this;
-
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/html', this.innerHTML);
-    }
-
-    function handleDragOver(e) {
-        if (e.preventDefault) {
+        }, true);
+        card.addEventListener('drag', function (e) {
             e.preventDefault();
-        }
+            if (isDown) {
+                card.style.left = (e.clientX + offset[0]) + 'px';
+                card.style.top = (e.clientY + offset[1]) + 'px';
+            }
 
-        e.dataTransfer.dropEffect = 'move';
-
-        return false;
-    }
-
-    function handleDragEnter(e) {
-        this.classList.add('over');
-    }
-
-    function handleDragLeave(e) {
-        this.classList.remove('over');
-    }
-
-    function handleDrop(e) {
-        if (e.stopPropagation) {
-            e.stopPropagation(); // stops the browser from redirecting.
-        }
-
-        if (dragSrcEl != this) {
-            dragSrcEl.innerHTML = this.innerHTML;
-            this.innerHTML = e.dataTransfer.getData('text/html');
-        }
-
-        return false;
-    }
-
-    function handleDragEnd(e) {
-        this.style.opacity = '1';
-
-        items.forEach(function (item) {
-            item.classList.remove('over');
-        });
-    }
-
-
-    let items = document.querySelectorAll('.container .card');
-    items.forEach(function (item) {
-        item.addEventListener('dragstart', handleDragStart, false);
-        item.addEventListener('dragenter', handleDragEnter, false);
-        item.addEventListener('dragover', handleDragOver, false);
-        item.addEventListener('dragleave', handleDragLeave, false);
-        item.addEventListener('drop', handleDrop, false);
-        item.addEventListener('dragend', handleDragEnd, false);
+        }, true);
+        card.addEventListener('dragend', function (e) {
+            isDown = false;
+            // console.log(offset,
+            //     [e.clientX + offset[0], e.clientY + offset[1]],
+            //     [e.clientX, e.clientY]);
+            card.style.left = (e.clientX + offset[0]) + 'px';
+            card.style.top = (e.clientY + offset[1]) + 'px';
+            card.classList.remove('dragging');
+        }, true);
     });
+
+    // let toggles = document.querySelectorAll('.toggle-card');
+    // toggles.forEach(function (toggle) {
+
+    //     toggle.addEventListener('click', toggleCard, true);
+
+    // });
+});
+
+// function activate(card, x, y) {
+//     let content = card.querySelector('.card-content');
+//     content.style.maxHeight = "fit-content";
+//     content.dataset.status = "active";
+// };
+
+// function toggleCard(e) {
+//     let card = e.target.parent;
+//     console.log(card);
+//     activate(card, e.clientX, e.clientY);
+//     if (e.target.dataset.status == "active") tail.dataset.status = "inactive";
+// }
+
+var $slider = document.getElementById('slider');
+var $toggle = document.getElementById('toggle');
+
+$toggle.addEventListener('click', function () {
+    var isOpen = $slider.classList.contains('slide-in');
+
+    $slider.setAttribute('class', isOpen ? 'slide-out' : 'slide-in');
 });
